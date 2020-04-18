635a7816020c6910ac7d0cdd7c6cd8e2369a0b40
Bocar <bocar..mbaye@gmail.com>
2020-04-18 20:29:13 +0100

    End of Section 4

---
+++ client-app/src/demo.ts
@@ -0,0 +1,27 @@
+let data = 42;
+
+data = 10;
+
+export interface ICar {
+    color: string;
+    model: string;
+    topSpeed?: number;
+}
+
+const car1 : ICar = {
+    color: 'blue',
+    model: 'BMW'
+}
+
+const car2: ICar = {
+    color: 'red',
+    model: 'Mercedes',
+    topSpeed: 100
+}
+
+const multiply = (x: number, y: number) =>
+{
+    return x * y;
+}
+
+export const cars = [car1, car2];
